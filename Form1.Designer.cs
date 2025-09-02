9698fed819417869aaf50b945351edb53b8c8612
ST10449392 <ST10449392@vcconnect.edu.za>
2025-09-02 23:21:51 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Fiblette
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWin = new System.Windows.Forms.Button();
+            this.btnLose = new System.Windows.Forms.Button();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.lblStartingAmount = new System.Windows.Forms.Label();
+            this.txtStartingAmount = new System.Windows.Forms.TextBox();
+            this.lblBalance = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnWin
+            // 
+            this.btnWin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnWin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWin.Location = new System.Drawing.Point(12, 363);
+            this.btnWin.Name = "btnWin";
+            this.btnWin.Size = new System.Drawing.Size(150, 75);
+            this.btnWin.TabIndex = 0;
+            this.btnWin.Text = "WIN";
+            this.btnWin.UseVisualStyleBackColor = false;
+            this.btnWin.Click += new System.EventHandler(this.btnWin_Click);
+            // 
+            // btnLose
+            // 
+            this.btnLose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnLose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLose.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLose.Location = new System.Drawing.Point(271, 363);
+            this.btnLose.Name = "btnLose";
+            this.btnLose.Size = new System.Drawing.Size(150, 75);
+            this.btnLose.TabIndex = 1;
+            this.btnLose.Text = "LOSE";
+            this.btnLose.UseVisualStyleBackColor = false;
+            this.btnLose.Click += new System.EventHandler(this.btnLose_Click);
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmount.Location = new System.Drawing.Point(12, 202);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(409, 158);
+            this.lblAmount.TabIndex = 2;
+            this.lblAmount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblStartingAmount
+            // 
+            this.lblStartingAmount.AutoSize = true;
+            this.lblStartingAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStartingAmount.Location = new System.Drawing.Point(12, 16);
+            this.lblStartingAmount.Name = "lblStartingAmount";
+            this.lblStartingAmount.Size = new System.Drawing.Size(155, 18);
+            this.lblStartingAmount.TabIndex = 3;
+            this.lblStartingAmount.Text = "Starting Amount: R ";
+            // 
+            // txtStartingAmount
+            // 
+            this.txtStartingAmount.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtStartingAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStartingAmount.Location = new System.Drawing.Point(173, 13);
+            this.txtStartingAmount.Name = "txtStartingAmount";
+            this.txtStartingAmount.Size = new System.Drawing.Size(248, 24);
+            this.txtStartingAmount.TabIndex = 4;
+            // 
+            // lblBalance
+            // 
+            this.lblBalance.AutoSize = true;
+            this.lblBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBalance.Location = new System.Drawing.Point(15, 54);
+            this.lblBalance.Name = "lblBalance";
+            this.lblBalance.Size = new System.Drawing.Size(78, 18);
+            this.lblBalance.TabIndex = 5;
+            this.lblBalance.Text = "Balance :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(433, 450);
+            this.Controls.Add(this.lblBalance);
+            this.Controls.Add(this.txtStartingAmount);
+            this.Controls.Add(this.lblStartingAmount);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.btnLose);
+            this.Controls.Add(this.btnWin);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fiblette";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWin;
+        private System.Windows.Forms.Button btnLose;
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.Label lblStartingAmount;
+        private System.Windows.Forms.TextBox txtStartingAmount;
+        private System.Windows.Forms.Label lblBalance;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fiblette
+{
+    public partial class Form1 : Form
+    {
+        private int fibIndex = 0;
+        private int[] fibSequence = new int[100];
+        private int bankroll = 100;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            createFibSequence();
+            resetFib();
+        }
+
+        private void createFibSequence()
+        {
+            fibSequence[0] = 1;
+            fibSequence[1] = 1;
+
+            for (int i = 2; i < fibSequence.Length; i++)
+            {
+                fibSequence[i] = fibSequence[i - 1] + fibSequence[i - 2];
+            }
+        }
+
+        private void resetFib()
+        {
+            fibIndex = 0;
+            lblBalance.Text = $"Balance: {bankroll}";
+            lblAmount.Text = $"Bet R {fibSequence[fibIndex].ToString()}";
+        }
+
+        private void btnWin_Click(object sender, EventArgs e)
+        {
+            bankroll += fibSequence[fibIndex] * 3;
+            resetFib();
+        }
+
+        private void btnLose_Click(object sender, EventArgs e)
+        {
+            bankroll -= fibSequence[fibIndex];
+            fibIndex++;
+            if (fibIndex >= fibSequence.Length)
+            {
+                fibIndex = fibSequence.Length - 1;
+            }
+            lblBalance.Text = $"Balance: {bankroll}";
+            lblAmount.Text = $"Bet R {fibSequence[fibIndex].ToString()}";
+        }
+
+    }
+}
